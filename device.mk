fd37b4fbc6c01794e73a68cad0772fecbc55a68e
ziachi <ziachi@users.noreply.github.com>
2026-06-20 05:50:45 +0700

    [v2] santoni: Fix bootloop - use AOSP power HAL (#7)
    Problem: system_server hangs waiting for android.hardware.power.IPower/default
    AIDL service. QTI power HAL AIDL v3 incompatible with MSM8937. Watchdog
    kills system_server after 3+ min → zygote restart → bootloop.
    
    Fix: Replace android.hardware.power-service-qti with
    android.hardware.power-service.example (AOSP stub HAL).
    Same concept proven working on MSM8937 in other projects.
    
    Files: device.mk
    Impact: Fixes bootloop, system boots to launcher

--- device.mk
+++ device.mk
@@ -393,7 +393,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.example \
     android.hardware.power@1.2.vendor \
     android.hardware.power@1.3.vendor
 
